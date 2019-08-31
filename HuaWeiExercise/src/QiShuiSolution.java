52368cf1d32b5b00eb5e15b8e6fa028629ff1a28
S-Cl-Ar-K-Ca <daxi_address@foxmail.com>
2019-08-31 11:39:26 +0800

    qishuiSolution

---
+++ HuaWeiExercise/src/QiShuiSolution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class QiShuiSolution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> inputs =new ArrayList<Integer>();
+        boolean exit = false;
+        while (!exit){
+            int in = scanner.nextInt();
+            inputs.add(in);
+            if (in == 0){
+                exit = true;
+                inputs.remove(inputs.size()-1);
+            }
+        }
+        for (Integer i:inputs) {
+            System.out.println(getResult(i));
+        }
+    }
+
+    public static int getResult(int a){
+        int res = 0;
+        if(a <= 1){
+            return res;
+        }else {
+            StringBuffer stringBuffer = new StringBuffer();
+            for(int i = 0 ;i < a; i++){
+                stringBuffer.append("1");
+            }
+            String s = stringBuffer.toString();
+            while (s.contains("111")){
+                res++;
+                s = s.replaceFirst("111","1");
+            }
+            if("11".equals(s)){
+                res++;
+            }
+        }
+        return res;
+    }
+}
