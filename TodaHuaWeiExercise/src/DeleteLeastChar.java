4a4e602b43ae48e542ccc247dbc7af772022b907
S-Cl-Ar-K-Ca <daxi_address@foxmail.com>
2019-09-08 14:46:10 +0800

    Merge0908

--- TodaHuaWeiExercise/src/DeleteLeastChar.java
+++ TodaHuaWeiExercise/src/DeleteLeastChar.java
@@ -1,10 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class DeleteLeastChar {
     public static void main(String[] args) throws IOException {
